c1b02fc0b80d6ef2ef823054da3ac8a712f9be4c
Marco Almada <43345162+MarcoDDM@users.noreply.github.com>
2023-11-04 17:55:56 -0300

    Adding two additional test cases

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,3 @@
-require 'rails_helper'
-
 RSpec.describe User, type: :model do
   it 'is invalid without a name' do
     user = User.new(posts_counter: 0)
@@ -9,5 +7,15 @@ RSpec.describe User, type: :model do
   it 'is invalid with a negative posts_counter' do
     user = User.new(name: 'Tom', posts_counter: -1)
     expect(user).to_not be_valid
+  end
+
+  it 'is valid with a zero posts_counter' do
+    user = User.new(name: 'Tom', posts_counter: 0)
+    expect(user).to be_valid
+  end
+
+  it 'is valid with a positive posts_counter' do
+    user = User.new(name: 'Tom', posts_counter: 1)
+    expect(user).to be_valid
   end
 end
