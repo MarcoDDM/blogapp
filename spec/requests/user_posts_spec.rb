6a439b179072d36e92ac7dc7aa2c9b7ad30ecf04
Marco Almada <43345162+MarcoDDM@users.noreply.github.com>
2023-11-06 17:43:56 -0300

    Add controller's specs

--- spec/requests/user_posts_spec.rb
+++ spec/requests/user_posts_spec.rb
@@ -1,7 +1,33 @@
 require 'rails_helper'
 
-RSpec.describe 'UserPosts', type: :request do
-  describe 'GET /index' do
-    pending "add some examples (or delete) #{__FILE__}"
+RSpec.describe UserPostsController, type: :request do
+  describe 'GET #index' do
+    it 'returns a successful response' do
+      get user_user_posts_path(user_id: 1)
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+  describe 'GET #show' do
+    it 'returns a successful response' do
+      get user_user_post_path(user_id: 1, id: 1)
+      expect(response).to have_http_status(:success)
+    end
+  end
+end
+
+RSpec.describe UsersController, type: :request do
+  describe 'GET #index' do
+    it 'returns a successful response' do
+      get users_path
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+  describe 'GET #show' do
+    it 'returns a successful response' do
+      get user_path(id: 1)
+      expect(response).to have_http_status(:success)
+    end
   end
 end
--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -1,7 +1,26 @@
 require 'rails_helper'
 
-RSpec.describe 'Users', type: :request do
-  describe 'GET /index' do
-    pending "add some examples (or delete) #{__FILE__}"
+RSpec.describe UsersController, type: :request do
+  describe 'Request spec for users index' do
+    it 'returns a successful response' do
+      puts "Checking HTTP status for users index..."
+      get '/users'
+      expect(response).to have_http_status(200)
+    end
   end
+
+  describe 'Request spec for users show' do
+    it 'returns a successful response' do
+      puts "Expecting to receive a successful response for users show..."
+      get '/users/:id'
+      expect(response).to have_http_status(200)
+    end
+
+    it 'includes the correct placeholder text in the response body' do
+      puts "Expecting to receive the correct placeholder text for users show..."
+      get '/users/:id'
+      expect(response.body).to include('User Profile')
+    end
+  end
+
 end
