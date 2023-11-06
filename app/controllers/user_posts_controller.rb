1313b8ad12171906b62814361d9eab6e3174d9dd
Marco Almada <43345162+MarcoDDM@users.noreply.github.com>
2023-11-06 14:31:27 -0300

    Update user posts controller

--- app/controllers/user_posts_controller.rb
+++ app/controllers/user_posts_controller.rb
@@ -1,2 +1,9 @@
 class UserPostsController < ApplicationController
+  def index
+    # Placeholder: List all posts by a given user
+  end
+
+  def show
+    # Placeholder: Display post details
+  end
 end
