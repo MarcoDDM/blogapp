beed0dddb0dd1cb48411b4b6e969bc89b05bc7eb
Marco Almada <43345162+MarcoDDM@users.noreply.github.com>
2023-11-06 20:26:35 -0300

    Update spec

--- spec/requests/user_posts_spec.rb
+++ spec/requests/user_posts_spec.rb
@@ -1,33 +1,71 @@
-require 'rails_helper'
-
 RSpec.describe UserPostsController, type: :request do
-  describe 'GET #index' do
+  describe 'Request spec for user posts index' do
     it 'returns a successful response' do
       get user_user_posts_path(user_id: 1)
       expect(response).to have_http_status(:success)
     end
+
+    it 'includes the correct placeholder text for user posts index' do
+      get user_user_posts_path(user_id: 1)
+      expect(response.body).to include('Welcome to user posts index page')
+    end
+
+    it 'renders the index template' do
+      get user_user_posts_path(user_id: 1)
+      expect(response).to render_template(:index)
+    end
   end
 
-  describe 'GET #show' do
+  describe 'Request spec for user posts show' do
     it 'returns a successful response' do
       get user_user_post_path(user_id: 1, id: 1)
       expect(response).to have_http_status(:success)
+    end
+
+    it 'includes the correct placeholder text for user posts show' do
+      get user_user_post_path(user_id: 1, id: 1)
+      expect(response.body).to include('Post Details')
+    end
+
+    it 'renders the show template' do
+      get user_user_post_path(user_id: 1, id: 1)
+      expect(response).to render_template(:show)
     end
   end
 end
 
 RSpec.describe UsersController, type: :request do
-  describe 'GET #index' do
+  describe 'Request spec for users index' do
     it 'returns a successful response' do
       get users_path
       expect(response).to have_http_status(:success)
     end
+
+    it 'includes the correct placeholder text for user index' do
+      get users_path
+      expect(response.body).to include('List of users')
+    end
+
+    it 'renders the index template' do
+      get users_path
+      expect(response).to render_template(:index)
+    end
   end
 
-  describe 'GET #show' do
+  describe 'Request spec for users show' do
     it 'returns a successful response' do
       get user_path(id: 1)
       expect(response).to have_http_status(:success)
+    end
+
+    it 'includes the correct placeholder text for user show' do
+      get user_path(id: 1)
+      expect(response.body).to include('User Profile')
+    end
+
+    it 'renders the show template' do
+      get user_path(id: 1)
+      expect(response).to render_template(:show)
     end
   end
 end
--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -3,23 +3,35 @@ require 'rails_helper'
 RSpec.describe UsersController, type: :request do
   describe 'Request spec for users index' do
     it 'returns a successful response' do
-      puts 'Checking HTTP status for users index...'
       get '/users'
       expect(response).to have_http_status(200)
     end
-  end
 
+    it 'includes the correct placeholder text for user index' do
+      get '/users'
+      expect(response.body).to include('List of users')
+    end
+
+    it 'renders the index template' do
+      get '/users'
+      expect(response).to render_template(:index)
+    end
+  end
+  
   describe 'Request spec for users show' do
     it 'returns a successful response' do
-      puts 'Expecting to receive a successful response for users show...'
       get '/users/:id'
       expect(response).to have_http_status(200)
     end
 
-    it 'includes the correct placeholder text in the response body' do
-      puts 'Expecting to receive the correct placeholder text for users show...'
+    it 'includes the correct placeholder text for user show' do
       get '/users/:id'
       expect(response.body).to include('User Profile')
+    end
+
+    it 'renders the show template' do
+      get '/users/:id'
+      expect(response).to render_template(:show)
     end
   end
 end
