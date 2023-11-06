f8109149a28192d5856897b3aee1ea48c1669201
Marco Almada <43345162+MarcoDDM@users.noreply.github.com>
2023-11-06 17:50:05 -0300

    Linters check

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.describe UsersController, type: :request do
   describe 'Request spec for users index' do
     it 'returns a successful response' do
-      puts "Checking HTTP status for users index..."
+      puts 'Checking HTTP status for users index...'
       get '/users'
       expect(response).to have_http_status(200)
     end
@@ -11,16 +11,15 @@ RSpec.describe UsersController, type: :request do
 
   describe 'Request spec for users show' do
     it 'returns a successful response' do
-      puts "Expecting to receive a successful response for users show..."
+      puts 'Expecting to receive a successful response for users show...'
       get '/users/:id'
       expect(response).to have_http_status(200)
     end
 
     it 'includes the correct placeholder text in the response body' do
-      puts "Expecting to receive the correct placeholder text for users show..."
+      puts 'Expecting to receive the correct placeholder text for users show...'
       get '/users/:id'
       expect(response.body).to include('User Profile')
     end
   end
-
 end
