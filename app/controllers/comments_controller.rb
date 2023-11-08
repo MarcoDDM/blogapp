e93e9bad2b4106277b9f971c56bff052a749f177
Marco Almada <43345162+MarcoDDM@users.noreply.github.com>
2023-11-08 15:08:48 -0300

    Create comments controller

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,22 @@
+class CommentsController < ApplicationController
+  def new
+    @comment = Comment.new
+  end
+
+  def create
+    @comment = Comment.new(comment_params)
+    @post = Post.find(params[:post_id])
+    @comment.author_id = current_user.id
+    @comment.post_id = @post.id
+
+    if @comment.save
+      redirect_to user_post_path(user_id: @post.author_id, id: @post.id)
+    else
+      render :new, alert: 'An error has occurred while creating the comment'
+    end
+  end
+
+  def comment_params
+    params.require(:comment).permit(:text)
+  end
+end
