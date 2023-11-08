0961e9a66723fcaec1479fb6a1d471db16421330
Marco Almada <43345162+MarcoDDM@users.noreply.github.com>
2023-11-08 14:56:06 -0300

    Update routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   root "users#index"
   resources :users, only: [:index, :show] do
-    resources :posts, only: [:index, :show]
+    resources :posts, only: [:index, :show, :new, :create] do
+      resources :comments, only: [:new, :create]
+      resources :likes, only: [:new, :create]
+    end
   end
 end
