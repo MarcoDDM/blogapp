4b2496997a0d482d6478913d36a5e9c5ceb24753
Marco Almada <43345162+MarcoDDM@users.noreply.github.com>
2023-11-14 16:38:43 -0300

    Update users post spec

--- spec/requests/user_posts_spec.rb
+++ spec/requests/user_posts_spec.rb
@@ -1,71 +1,65 @@
-RSpec.describe UserPostsController, type: :request do
-  describe 'Request spec for user posts index' do
-    it 'returns a successful response' do
-      get user_user_posts_path(user_id: 1)
-      expect(response).to have_http_status(:success)
-    end
+require 'rails_helper'
 
-    it 'includes the correct placeholder text for user posts index' do
-      get user_user_posts_path(user_id: 1)
-      expect(response.body).to include('Welcome to user posts index page')
-    end
-
-    it 'renders the index template' do
-      get user_user_posts_path(user_id: 1)
-      expect(response).to render_template(:index)
-    end
+RSpec.describe 'Post Index', type: :system do
+  before do
+    driven_by(:rack_test)
   end
 
-  describe 'Request spec for user posts show' do
-    it 'returns a successful response' do
-      get user_user_post_path(user_id: 1, id: 1)
-      expect(response).to have_http_status(:success)
-    end
-
-    it 'includes the correct placeholder text for user posts show' do
-      get user_user_post_path(user_id: 1, id: 1)
-      expect(response.body).to include('Post Details')
-    end
-
-    it 'renders the show template' do
-      get user_user_post_path(user_id: 1, id: 1)
-      expect(response).to render_template(:show)
-    end
-  end
-end
-
-RSpec.describe UsersController, type: :request do
-  describe 'Request spec for users index' do
-    it 'returns a successful response' do
-      get users_path
-      expect(response).to have_http_status(:success)
-    end
-
-    it 'includes the correct placeholder text for user index' do
-      get users_path
-      expect(response.body).to include('List of users')
-    end
-
-    it 'renders the index template' do
-      get users_path
-      expect(response).to render_template(:index)
-    end
+  let(:user) do
+    User.create(name: 'Adnan', photo: 'https://unsplash.com/photos/ABC123', posts_counter: 5)
   end
 
-  describe 'Request spec for users show' do
-    it 'returns a successful response' do
-      get user_path(id: 1)
-      expect(response).to have_http_status(:success)
-    end
+  let!(:post) do
+    Post.create(title: 'My First Post', text: 'This is my first post', author: user, comments_counter: 0, likes_counter: 0)
+  end
 
-    it 'includes the correct placeholder text for user show' do
-      get user_path(id: 1)
-      expect(response.body).to include('User Profile')
-    end
+  let!(:comment) do
+    Comment.create(text: 'This is my first comment', author: user, post: post)
+  end
 
-    it 'renders the show template' do
-      get user_path(id: 1)
-      expect(response).to render_template(:show)
-    end
+  it "displays the user's profile picture on user posts page" do
+    visit users_path(user_id: user.id)
+    expect(page).to have_css("img[src='#{user.photo}']")
+  end
+
+  it "displays the user's name on user posts page" do
+    visit users_path(user_id: user.id)
+    expect(page).to have_content(user.name)
+  end
+
+  it "displays the user's post count on user posts page" do
+    visit users_path(user_id: user.id)
+    expect(page).to have_content(user.posts_counter)
+  end
+
+  it 'displays a post title on user posts page' do
+    visit user_posts_path(user_id: user.id)
+    expect(page).to have_content(post.title)
+  end
+
+  it 'displays a post text on user posts page' do
+    visit user_posts_path(user_id: user.id)
+    expect(page).to have_content(post.text)
+  end
+
+  it 'displays the first comments on a post' do
+    visit user_posts_path(user_id: user.id)
+    expect(page).to have_content(comment.text)
+  end
+
+  it 'displays the number of comments on a post' do
+    visit user_posts_path(user_id: user.id)
+    expect(page).to have_content(post.comments_counter)
+  end
+
+  it 'displays the number of likes on a post' do
+    visit user_posts_path(user_id: user.id)
+    expect(page).to have_content(post.likes_counter)
+  end
+
+  it 'redirects to post show page when clicking on post title' do
+    visit user_posts_path(user_id: user.id)
+    click_link post.title
+    expect(page).to have_current_path(user_post_path(user_id: user.id, id: post.id))
   end
 end
