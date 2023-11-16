e89bf58127fa6df9bb3808a105becdf4319f879d
SFarida <faridagueleu@gmail.com>
2023-11-16 21:47:23 +0100

    Update roles

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -1,16 +1,17 @@
 class Ability
-  include CanCan::Ability
-
-  def initialize(user)
-    # Define abilities for the user (current_user is passed as 'user')
-    user ||= User.new # Guest user (not logged in)
-
-    # Define abilities for posts
-    can :destroy, Post, user_id: user.id
-    can :destroy, Post if user.role == 'admin'
-
-    # Define abilities for comments
-    can :destroy, Comment, user_id: user.id
-    can :destroy, Comment if user.role == 'admin'
-  end
+  include CanCan::Ability  def initialize(user)
+  # Define abilities for the passed in user here. For example:
+  if user.role == 'admin'
+    can :manage, :all
+  else
+    can :read, :all
+    can :create, Post, author_id: user.id
+    can :create, Comment, author_id: user.id
+    can :create, Like
+    can :destroy, Post do |post|
+      post.author_id == user.id
+    end
+    can :destroy, Comment do |comment|
+      comment.author_id == user.id
+    end
 end
