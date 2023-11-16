d99052fbbc7ba5d50516ca5f0d3abbd5c3090fc1
Marco Almada <43345162+MarcoDDM@users.noreply.github.com>
2023-11-16 18:15:37 -0300

    Update sender email

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,6 +1,9 @@
 require "active_support/core_ext/integer/time"
 
 Rails.application.configure do
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  config.action_mailer.delivery_method = :letter_opener
+  config.action_mailer.perform_deliveries = true
   # Settings specified here will take precedence over those in config/application.rb.
 
   # In the development environment your application's code is reloaded any time
@@ -13,7 +16,7 @@ Rails.application.configure do
 
   # Show full error reports.
   config.consider_all_requests_local = true
-
+  config.action_mailer.raise_delivery_errors = true
   # Enable server timing
   config.server_timing = true
 
@@ -37,10 +40,9 @@ Rails.application.configure do
   config.active_storage.service = :local
 
   # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
 
   config.action_mailer.perform_caching = false
-
+  
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
 
