0a8b87ab13d927b5e6df0a8e0cb1d853fc8766a5
Marco Almada <43345162+MarcoDDM@users.noreply.github.com>
2023-11-17 11:41:56 -0300

    Check linters

--- test/controllers/api/v1/users_controller_test.rb
+++ test/controllers/api/v1/users_controller_test.rb
@@ -1,7 +1,7 @@
-require "test_helper"
+require 'test_helper'
 
 class Api::V1::UsersControllerTest < ActionDispatch::IntegrationTest
-  test "should get index" do
+  test 'should get index' do
     get api_v1_users_index_url
     assert_response :success
   end
