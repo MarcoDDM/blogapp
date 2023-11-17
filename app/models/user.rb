de37f0db4a4b181f42ef4197f363462e4852ed09
Marco Almada <43345162+MarcoDDM@users.noreply.github.com>
2023-11-17 11:24:32 -0300

    Api token for user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, :confirmable
   has_many :posts, foreign_key: :author_id, dependent: :destroy
@@ -6,8 +8,9 @@ class User < ApplicationRecord
   has_many :likes, foreign_key: :author_id
 
   validates :name, presence: true, allow_blank: true
-  validates :email, presence: true, uniqueness: true
   validates :posts_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0, allow_nil: true }
+
+  before_save :generate_api_token
 
   def recent_posts
     posts.order(created_at: :desc).limit(3)
@@ -19,5 +22,11 @@ class User < ApplicationRecord
 
   def update_comments_counter
     update(comments_counter: comments.count)
+  end
+
+  private
+
+  def generate_api_token
+    self.api_token = SecureRandom.hex(16)
   end
 end
